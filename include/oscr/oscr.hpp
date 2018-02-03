82cfb1ee003a37cd8668d15a4116818f50e330a3
Oscar E. Ramos <15218582+oscar-ramos@users.noreply.github.com>
2018-02-03 00:48:38 -0500

    Add header to include other headers

---
+++ include/oscr/oscr.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017, Oscar Ramos
+ *
+ * This file is part of oscr.
+ *
+ * oscr is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * oscr is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with oscr. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _OSCR_HPP_
+#define _OSCR_HPP_
+
+#include <model/robot-model.hpp>
+#include <model/robot-model-pin.hpp>
+#include <model/robot-model-rbdl.hpp>
+
+#include <tools/math-utils.hpp>
+#include <tools/model-utils.hpp>
+
+#include <ik/kine-task-com.hpp>
+#include <ik/kine-task.hpp>
+#include <ik/kine-task-pose.hpp>
+#include <ik/kine-task-posture.hpp>
+#include <ik/osik-solvers.hpp>
+
+#endif
