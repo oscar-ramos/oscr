9e6dc9431131fd1c091a45ddcc770eba16832b8d
Oscar E. Ramos <15218582+oscar-ramos@users.noreply.github.com>
2018-10-14 17:21:09 -0500

    Add basic kinematic computations to the example

--- examples/c++/ff6dof-model.cpp
+++ examples/c++/ff6dof-model.cpp
@@ -1,26 +1,57 @@
 /*
- * Test the parsing of the model of a simple revolute robot with a free floating
- * base and 6 dof (2 chains of 3 dof each)
+ * Test the model parsing and some basic kinematic properties of a simple
+ * revolute robot with a free floating base and 6 dof (2 chains of 3 dof each)
+ *
+ * To execute:
+ *      ./ff6dofModel backend - where backend can be pin (by default) or rbdl 
+ * 
  */
 
 #include <oscr/model/robot-model-pin.hpp>
+#include <oscr/model/robot-model-rbdl.hpp>
 #include <oscr/tools/model-utils.hpp>
 
 
 int main(int argc, char *argv[])
 {
-  // Whether floating base is used or not
-  bool fbase = false;
+  // Use a floating base?
+  bool fbase = true;
+  // Robot model
   std::string modelname = "../models/ff6dof.urdf";
-
-  oscr::RobotModel *robot = new oscr::RobotModelPin(modelname, fbase);
-
+  // Dummy initialization
+  oscr::RobotModel *robot = new oscr::RobotModelPin();
+  
+  if (argc<2)
+  {
+    std::cout << "-- Using Pinocchio as backend by default" << std::endl;
+    robot = new oscr::RobotModelPin(modelname, fbase);
+  }
+  else
+  {
+    if (!strcmp(argv[1],"pin"))
+    {
+      std::cout << "-- Using Pinocchio as backend" << std::endl;
+      robot = new oscr::RobotModelPin(modelname, fbase);
+    }
+    else if (!strcmp(argv[1], "rbdl"))
+    {
+      std::cout << "-- Using RBDL as backend" << std::endl;
+      robot = new oscr::RobotModelRbdl(modelname, fbase);
+    }
+    else
+    {
+      std::cout << "Only pin and rbdl are accepted as backends ..."
+                << "ending the program ..." << std::endl;
+      return -1;
+    }
+  }
+    
   unsigned int ndof, ndof_actuated;
   bool has_floating_base;
   std::string floating_link;
   Eigen::VectorXd qmin, qmax, dqmax;
   std::vector<std::string> jnames;
-  std::map<std::string, unsigned int> maplink_name_ID;
+  std::map<std::string, unsigned int> linkID;
 
   // Number of Degrees of Freedom
   ndof  = robot->ndof();
@@ -36,7 +67,7 @@ int main(int argc, char *argv[])
   // Jont names
   jnames = robot->jointNames();
   // Link names and IDs
-  maplink_name_ID = robot->mapLinkNamesIDs();
+  linkID = robot->mapLinkNamesIDs();
 
   // Show the previous information on the screen
   std::cout << "Total ndof: " << ndof << std::endl;
@@ -44,7 +75,30 @@ int main(int argc, char *argv[])
   std::cout << "Has floating base: " << has_floating_base << std::endl;
   std::cout << "Floating link: " << floating_link << std::endl;
   oscr::printJointLimits(jnames, qmin, qmax, dqmax);
-  oscr::printLinkID(maplink_name_ID);
+  oscr::printLinkIDshort(linkID);
+
+  // Joint configuration with all values to zero.
+  Eigen::VectorXd q(ndof), qcurrent(ndof); q.setZero();
+  // If there is a floating base, then set a valid null quaternion
+  if (has_floating_base)
+    q[3]=1.0;
+  // Set the current joint configuration
+  robot->updateJointConfig(q);
+  
+  // Get the current joint configuration
+  qcurrent = robot->getJointConfig();
+
+  // Get the pose of larm3 and lleg3 and the Jacobian of larm3
+  Eigen::VectorXd poselarm, poselleg;
+  Eigen::MatrixXd Jlarm;
+  poselarm = robot->linkPose(linkID["larm3"]);
+  poselleg = robot->linkPose(linkID["lleg3"]);
+  Jlarm = robot->geometricJacobian(linkID["larm3"]);
+    
+  std::cout << "\nCurrent joint config: " << qcurrent.transpose() << std::endl;
+  std::cout << "Pose of larm3: " << poselarm.transpose() << std::endl;
+  std::cout << "Pose of lleg3: " << poselleg.transpose() << std::endl;
+  std::cout << "Geometric Jacobian of larm3:\n " << Jlarm << std::endl;
 
   return 0;
 }
